139d5abe94b963bfb982e40062be9fd896577630
Aviral <aviral1101@gmail.com>
2022-02-12 01:16:54 +0530

    backend Fired

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const route = require("./routes/routes.js");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose.connect(
+  "mongodb+srv://halservices:harshluvjeet10@cluster0.o0zhh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+app.use("/", route);
+
+app.listen(5000, (req, res) => {
+  console.log("Server is up and running at Port 5000");
+});
