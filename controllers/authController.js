7d57126331241c5e311f0ab877a176fd04dcb264
harshhx <hg070401@gmail.com>
2022-02-12 21:23:11 +0530

    user_schema test

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,14 +8,6 @@ module.exports.signUp_post = async (req,res)=>{
         const user = await User.create({username: username, phone_number:phone_number, email:email});
         res.status(200).json({id:user._id})
 
-        // user.save((err)=>{
-        //     if (err){
-        //         res.sendStatus(400).send({error:err.message});
-        //     }
-        //     else{
-        //         res.sendStatus(200).send(user)
-        //     }
-        // });
     }
     catch(err){
         res.sendStatus(400).send({error:err.message});
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const express=require('express')
 const router=express.Router()
 const authController = require('../controllers/authController')
+
 router.get('/',(req,res)=>{
     res.send("hello world")
-})
+});
 
 router.post("/signup", authController.signUp_post);
 
