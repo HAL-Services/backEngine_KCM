b8a70ce5697df9767a707cbe8e1a19aad985048c
Aviral <aviral1101@gmail.com>
2022-02-12 23:24:33 +0530

    updates

--- models/user_schema.js
+++ models/user_schema.js
@@ -1,23 +1,26 @@
-const mongoose = require("mongoose")
-const {isEmail} = require('validator')
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    phone_number:{
-        type: Number,
-        required: true,
-        unique: true,
+    phone_number: {
+      type: Number,
+      required: true,
+      unique: true,
     },
-    email:{
-        type: String,
-        validate: [ isEmail, 'invalid email' ],
-        unique: true,
-        trim:true,
+    email: {
+      type: String,
+      validate: [isEmail, "invalid email"],
+      unique: true,
+      trim: true,
     },
-},{timestamps: true})
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema)
-module.exports = User
+const User = mongoose.model("User", userSchema);
+module.exports = User;
