6917b3770408381d1a08fee43bf57524165693ad
harshhx <hg070401@gmail.com>
2022-02-15 01:11:55 +0530

    find user by phone

--- controllers/userDataController.js
+++ controllers/userDataController.js
@@ -7,11 +7,21 @@ module.exports.all_users = async (req, res)=>{
         data.push(user._doc);
     })
     if (data.length>0){
-        res.status(200).send({data:data})
+        res.status(200).send({data:data, data_found:true})
     }
     else{
-        res.status(500).send({msg:"Invalid query", data:[]})
+        res.status(500).send({msg:"Invalid query", data:[], data_found:false})
     }
 
 }
 
+module.exports.userPhone = async (req, res) =>{
+    const phone = req.body.phone;
+    const query = await User.findOne({phone_number:phone});
+    if (query === null){
+        res.status(500).send({msg:"Record not found", data_found:false })
+    }
+    else {
+        res.status(200).send({data: query._doc, data_found:true});
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,6 @@ const userDataController = require('../controllers/userDataController')
 
 
 router.get('/all/', userDataController.all_users);
-
+router.get('/phone/', userDataController.userPhone);
 
 module.exports=router
