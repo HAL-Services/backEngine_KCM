c972693bc1e84c0f6f822c1fb7460c98e23a050b
Aviral <aviral1101@gmail.com>
2022-08-30 16:08:07 +0530

    removed twilio dependencies and add get user details

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,16 @@
 const User = require("../models/user_schema");
 const bcrypt = require("bcryptjs");
-
+module.exports.getUserDetails = async (req, res) => {
+  try {
+    const id = req.user._id.toHexString();
+    const username = req.user.username;
+    const email = req.user.email;
+    const mobile = req.user.mobile;
+    res.status(200).send({ id, username, email, mobile });
+  } catch (err) {
+    res.status(404).send({ error: err.message, message: "User not found" });
+  }
+};
 module.exports.updateProfile = async (req, res) => {
   try {
     const id = req.user._id.toHexString();
