71818c8cd29094ff3b43cb166b75b3c929333e34
Aviral <aviral1101@gmail.com>
2022-08-30 08:57:00 +0530

    login routes made and signup corrected

--- models/user_schema.js
+++ models/user_schema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema(
@@ -7,7 +8,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    phone_number: {
+    mobile: {
       type: Number,
       required: true,
       unique: true,
@@ -18,10 +19,35 @@ const userSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    password: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
 
+// login method
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+  return user;
+};
+
+// hasing password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
